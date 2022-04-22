7853f3ff24a268f0aab59e0792ef25e8b780fb24
Felipe Cabral <felipecabral33@gmail.com>
2022-04-22 18:25:30 -0300

    BootCamp Carrefour Web Developer DIO - Recriando a Interface do NETFLIX

--- modulo2-html-css3/04-recriando-a-interface-do-netflix/js/main.js
+++ modulo2-html-css3/04-recriando-a-interface-do-netflix/js/main.js
@@ -1,7 +1,7 @@
-$('.owl-carousel').owlCarousel({
+$('.destaque').owlCarousel({
     loop: true,
     margin: 15,
-    nav: true,
+    dots: false,
     responsive: {
         0: {
             items: 1
@@ -14,3 +14,44 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$('.top').owlCarousel({
+    loop: false,
+    margin: 15,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+var vid = document.getElementById("trailer");
+
+$(window).scroll(function () {
+    $('header .container').toggleClass('scrolled', $(this).scrollTop() > 50);
+
+});
+
+function mute() {
+    vid.muted = true;
+    $('.fa-solid.fa-volume-high').hide();
+    $('.fa-solid.fa-volume-xmark').show();
+}
+function som() {
+    vid.muted = false;
+    $('.fa-solid.fa-volume-high').show();
+    $('.fa-solid.fa-volume-xmark').hide();
+}
+
+$(window).scroll(function () {
+    vid.play();
+    vid.volume = 0.5;
+});
