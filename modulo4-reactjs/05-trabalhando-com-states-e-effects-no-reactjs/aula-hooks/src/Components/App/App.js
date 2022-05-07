8d4407b748099a5679a76dfb1faf96193cfab1a3
Felipe Cabral <felipecabral33@gmail.com>
2022-05-07 15:03:41 -0300

    BootCamp Carrefour Web Developer DIO - Modulo IV ReactJs

---
+++ modulo4-reactjs/05-trabalhando-com-states-e-effects-no-reactjs/aula-hooks/src/Components/App/App.js
@@ -0,0 +1,16 @@
+import Counter from '../Counter/Counter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
+import SmartCounter from '../Smartcounter/Smartcounter';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Ifoodcounter />
+    </>
+  );
+}
+
+export default App;
