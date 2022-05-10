1aa5b0949a29b3cc6fea29ff006aa4145fb2f012
Felipe Cabral <felipecabral33@gmail.com>
2022-05-10 18:31:48 -0300

    Introdução ao ecosistema Java

--- modulo5-java-springboot/01-introducao-ao-ecossistema-e-documentacao-java/01-introducao-ao-ecosistema-java/src/com/dio/MyFirstProgram.java
+++ modulo5-java-springboot/01-introducao-ao-ecossistema-e-documentacao-java/01-introducao-ao-ecosistema-java/src/com/dio/MyFirstProgram.java
@@ -4,7 +4,7 @@ import com.dio.base.Order;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        Order order = new Order("code1234");
+        Order order = new Order("Teste", 99.99);
         System.out.println(order);
     }
 }
--- modulo5-java-springboot/01-introducao-ao-ecossistema-e-documentacao-java/01-introducao-ao-ecosistema-java/src/com/dio/base/Order.java
+++ modulo5-java-springboot/01-introducao-ao-ecossistema-e-documentacao-java/01-introducao-ao-ecosistema-java/src/com/dio/base/Order.java
@@ -2,15 +2,29 @@ package com.dio.base;
 
 public class Order {
     private final String code;
+    private final double totalValue;
 
-    public Order(String code) {
+    public Order(String code, double totalValue) {
         this.code = code;
+        this.totalValue = totalValue;
     }
 
-    @Override
-    public String toString() {
-        return "Order={" +
-                "code='" + code + "'" +
-                "}";
+    public double calculateFee() {
+        if (totalValue > 100) {
+            return totalValue * 0.99;
+        } else {
+            return totalValue;
+        }
     }
+
+    /*
+     * Exemplo Aula 1
+     * 
+     * @Override
+     * public String toString() {
+     * return "Order={" +
+     * "code='" + code + "'" +
+     * "}";
+     * }
+     */
 }
