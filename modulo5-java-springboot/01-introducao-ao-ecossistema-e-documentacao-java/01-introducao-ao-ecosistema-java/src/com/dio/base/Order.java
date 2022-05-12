0ba300334b66da8dc8533ded82f244680baf0e3a
Felipe Cabral <felipecabral33@gmail.com>
2022-05-12 00:45:04 -0300

    Modulo V - Java/SpringBoot

--- modulo5-java-springboot/01-introducao-ao-ecossistema-e-documentacao-java/01-introducao-ao-ecosistema-java/src/com/dio/base/Order.java
+++ modulo5-java-springboot/01-introducao-ao-ecossistema-e-documentacao-java/01-introducao-ao-ecosistema-java/src/com/dio/base/Order.java
@@ -1,32 +1,74 @@
 package com.dio.base;
 
+/**
+ * @author Felipe Cabral
+ * @version 1.0.0
+ * @since 1.0
+ */
+
 public class Order {
+    // Representa o código do pedido
     private final String code;
     private final double totalValue;
 
+    private String[] itens;
+
+    /**
+     * Método construtor
+     * Construtor da Classe Order
+     * 
+     * @param code       // Código de pedido
+     * @param totalValue // Valor total do pedido
+     */
     public Order(String code, double totalValue) {
         this.code = code;
         this.totalValue = totalValue;
     }
 
-    public double calculateFee() {
-        if (totalValue > 100) {
-            return totalValue * 2.99;
-        } else {
-            return totalValue;
+    public void printIntems() {
+        int i = 0;
+        while (i < itens.length) {
+            System.out.println("Usuando WHILE: " + itens);
+            i++;
         }
+        int a = 0;
+        do {
+            System.out.println("Usuando DO: " + itens);
+            a++;
+        } while (a < itens.length);
+
+        for (int c = 0; c < itens.length; i++) {
+            System.out.println("Usuando FOR: " + itens);
+        }
+        for (String d : itens) {
+            System.out.println("Usuando FOR: " + d);
+        }
+
     }
 
-    /*
-     * Exemplo Aula 1
+    /**
+     * Calcula valor total de acordo com o pedido.
+     * Se valor maior que 100, taxa será aplicada.
      */
-
-    @Override
-    public String toString() {
-        return "Order={" +
-                "code='" + code + "'" +
-                "totalValue='" + totalValue + "'" +
-                "}";
-    }
-
+    /*
+     * public double calculateFee() {
+     * if (totalValue > 100) {
+     * return totalValue * 2.99;
+     * } else {
+     * return totalValue;
+     * }
+     * }
+     * 
+     * 
+     * Exemplo Aula 1
+     * 
+     * 
+     * @Override
+     * public String toString() {
+     * return "Order={" +
+     * "code='" + code + "'" +
+     * "totalValue='" + totalValue + "'" +
+     * "}";
+     * }
+     */
 }
