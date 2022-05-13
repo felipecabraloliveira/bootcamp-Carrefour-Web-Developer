72e3397cf45a3d2110c2c2fab4143223dbdd0d9e
Felipe Cabral <felipecabral33@gmail.com>
2022-05-13 16:50:56 -0300

    Módulo V - Java/SpringBoot - Primeiro Programa Java na IDE IntelliJ.

--- modulo5-java-springboot/02-dominando-ides-java/05-intellij/primeiro-programa-intellij/src/com/dio/PrimeiroPrograma.java
+++ modulo5-java-springboot/02-dominando-ides-java/05-intellij/primeiro-programa-intellij/src/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package com.dio;
 
 import com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato("Jake", "Rajadinho", 1);
@@ -9,5 +11,55 @@ public class PrimeiroPrograma {
         int b = 3;
         System.out.println("Hello World "+ (a+b));*/
         System.out.println(gato);
+
+        Livro livro1 = new Livro("Harry Potter e o Prisioneiro de Azkaban", 288);
+        System.out.println(livro1);
     }
 }
+    class Livro {
+        private String nome;
+        private Integer numPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Livro livro = (Livro) o;
+            return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nome, numPaginas);
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
+    }
+
