9f71a4f463f86cb223fb445d49aaa05bd456b6f8
Felipe Cabral <felipecabral33@gmail.com>
2022-05-13 16:16:47 -0300

    Módulo V - Java/SpringBoot - Primeiro Programa Java na IDE IntelliJ.

---
+++ modulo5-java-springboot/02-dominando-ides-java/05-intellij/primeiro-programa-intellij/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Jake", "Rajadinho", 1);
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World "+ (a+b));*/
+        System.out.println(gato);
+    }
+}
