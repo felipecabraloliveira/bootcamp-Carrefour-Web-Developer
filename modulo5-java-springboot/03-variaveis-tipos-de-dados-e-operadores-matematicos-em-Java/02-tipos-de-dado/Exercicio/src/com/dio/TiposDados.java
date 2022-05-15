a2d2ebb242c095d3de0a070644d462cd4365943c
Felipe Cabral <felipecabral33@gmail.com>
2022-05-15 18:05:31 -0300

    Bootcamp Carrefour WebDeveloper - Modulo V - Java/SpringBoot - Curso de tipos de dados

---
+++ modulo5-java-springboot/03-variaveis-tipos-de-dados-e-operadores-matematicos-em-Java/02-tipos-de-dado/Exercicio/src/com/dio/TiposDados.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+public class TiposDados{
+        public static void main(String[]args){
+                int a = 20;
+                double af= 3.02;
+                float f2 = 3.40f;
+
+                double d1 = 5.10;
+                double d2 = 5.10d;
+
+                char c1 = 'W';
+
+                String st1 = "Contrato";
+                String nomeCompleto = "Felipe Cabral de Oliveira";
+                final int NUMERO_TENTATIVAS = 5;
+
+                boolean bol1 = true;
+                boolean bol2 = false;
+
+                System.out.println(bol1);
+        }
+}
