889e36e3dec0f584a89774e41357a185e16d68ad
Felipe Cabral <felipecabral33@gmail.com>
2022-05-15 22:52:15 -0300

    Bootcamp Carrefour WebDeveloper - Módulo V - Java/SpringBoot, Trabalhando com retornos de métodos.

---
+++ modulo5-java-springboot/04-entendendo-metodos-java/03-retornos/Calculadora/src/com/dio/Calculadora.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+public class Calculadora {
+
+    public static double adicao(double a, double b){
+        return a + b;
+    }
+    public static double subtracao(double a, double b){
+        return a - b;
+    }
+    public static double divisao(double a, double b){
+        return a / b;
+    }
+    public static double multiplicacao(double a, double b){
+        return a * b;
+    }
+
+
+}
