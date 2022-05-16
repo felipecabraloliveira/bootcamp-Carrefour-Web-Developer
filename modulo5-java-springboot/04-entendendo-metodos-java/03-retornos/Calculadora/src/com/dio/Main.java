6bc24ce626a09a7be71ede1f9d234c0e0a39aad0
Felipe Cabral <felipecabral33@gmail.com>
2022-05-15 22:53:23 -0300

    Bootcamp Carrefour WebDeveloper - Módulo V - Java/SpringBoot

---
+++ modulo5-java-springboot/04-entendendo-metodos-java/03-retornos/Calculadora/src/com/dio/Main.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 10;
+        double num2 = 5;
+        System.out.println("Soma: " + Calculadora.adicao(num1,num2));
+        System.out.println("Subtração: " + Calculadora.subtracao(num1,num2));
+        System.out.println("Divisão: " + Calculadora.divisao(num1,num2));
+        System.out.println("Multiplicação: " + Calculadora.multiplicacao(num1,num2));
+
+    }
+}
+
