3602bc49c33f73b050a299a62c799ca51f12a81e
Felipe Cabral <felipecabral33@gmail.com>
2022-05-17 18:37:00 -0300

    Modulo V - Java/SpringBoot - POO Programação Orientada a Objetos

---
+++ modulo5-java-springboot/07-programacao-orientada-a-objetos/04-estrutura-basica-da-oo/02 - classe-exercicio/src/com/dio/Carro.java
@@ -0,0 +1,42 @@
+package com.dio;
+
+// Criar a classe Carro
+class Carro {
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    public Carro() {
+        super();
+    }
+
+    public Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+}
