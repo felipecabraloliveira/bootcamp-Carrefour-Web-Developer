40e1650117215bcfbb0bd229add5b83bca40eb7b
Felipe Cabral <felipecabral33@gmail.com>
2022-05-19 16:11:13 -0300

    Modulo V - Java/SpringBoot - Debugging Java

--- modulo5-java-springboot/08-debugging-java/02-pilha-de-execucao-de-um-projeto-java/pilha-de-execucao/src/com/dio/debbuging/CalculadoraMedia.java
+++ modulo5-java-springboot/08-debugging-java/02-pilha-de-execucao-de-um-projeto-java/pilha-de-execucao/src/com/dio/debbuging/CalculadoraMedia.java
@@ -1,2 +1,25 @@
-package com.dio.debbuging;public class CalculadoraMedia {
+package com.dio.debbuging;
+
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        //Chamando o método CalculaMedia
+        double media = calculaMedia(alunos, scan);
+
+        System.out.printf("Média da turma %.1f", media);
+    }
+    public static double calculaMedia(String[] alunos, Scanner scanner) {
+
+        double soma = 0;
+        for(String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+        return soma / alunos.length;
+    }
 }
