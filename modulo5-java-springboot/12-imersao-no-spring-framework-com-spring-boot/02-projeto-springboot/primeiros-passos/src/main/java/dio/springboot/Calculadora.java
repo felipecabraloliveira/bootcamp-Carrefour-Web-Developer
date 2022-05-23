0a4e0e12272e9f6a00c9e17728d07d3634020fb9
Felipe Cabral <felipecabral33@gmail.com>
2022-05-23 15:31:55 -0300

    Modulo V - Java/SpringBoot - SpingBoot Imersão

---
+++ modulo5-java-springboot/12-imersao-no-spring-framework-com-spring-boot/02-projeto-springboot/primeiros-passos/src/main/java/dio/springboot/Calculadora.java
@@ -0,0 +1,12 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+
+    public int somar(int a, int b){
+        return a + b;
+    }
+}
